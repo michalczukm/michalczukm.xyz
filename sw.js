4106961330cfec3466aae7e97b67b6ca797c8cf5
Michał Michalczuk <michalczukm@gmail.com>
2019-09-01 23:38:29 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7cfc7d01c6f5d7b49854.js"
+    "url": "webpack-runtime-e02523fbe5fd68a94b12.js"
   },
   {
     "url": "app-68899542ad83125e937f.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "532c8e079ceb46b28491bf835ae4997d"
+    "revision": "b6bf33b8f95391f31e4293c36be310a7"
   },
   {
     "url": "styles.c23ff14d1d854b8da124.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "018192e8d5175a72d7cb1223e532e1fb"
+    "revision": "f9334640ad139aba12c28e558931cd56"
   },
   {
     "url": "manifest.webmanifest",
