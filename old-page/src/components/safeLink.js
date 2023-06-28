ca9cac01225dfce9e65a72bd04132ddf1183f1ee
Michal Michalczuk <michalczukm@gmail.com>
2023-06-28 03:05:50 +0200

    remove old one

--- old-page/src/components/safeLink.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default ({href, children, className, title}) => (
-  <a href={href} className={className} target="_blank" rel="noopener noreferrer" title={title} >
-    {children}
-  </a>
-)
